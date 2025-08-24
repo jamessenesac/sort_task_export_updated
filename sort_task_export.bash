f58ce49a180982b1da2cb9561f8e391429671f04
james senesac <61091813+jamessenesac@users.noreply.github.com>
2025-08-23 20:50:07 -0700

    Update sort_task_export.bash

--- sort_task_export.bash
+++ sort_task_export.bash
@@ -1,67 +1,56 @@
 #!/bin/bash
 
 # move the extraneous files out of the way
-
-mkdir blah; 
-
-mv application* blah/; 
-
-mv index.html blah/; 
-
-mv jquery.js blah/; 
-
+mkdir -p blah
+mv application* blah/
+mv index.html blah/
+mv jquery.js blah/
 mv bootstrap* blah/
 
+# move the successful/failed jobs out of the way
+mkdir -p failure
 
+# For each *.html file, find failures (warning|error) and move them to failure/
+for i in $(for j in $(ls); do grep -E -A 1 'Result:' "$j" 2>/dev/null -H | grep -E 'warning|error' | awk '{print $1}' | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g | sort -u; done); do
+  mv "$i" failure/
+done
 
-# move the successful jobs out of the way
+mkdir -p success
 
-mkdir failure
-
-#for i in `egrep -A 1 Result: * 2>/dev/null | egrep "warning|error" | awk '{print $1}' | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g | sort -u`; do mv $i failure/; done
-for i in `for j in $(ls); do egrep -A 1 Result: $j 2>/dev/null -H | egrep "warning|error" | awk '{print $1}' | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g | sort -u; done`; do mv $i failure/; done
-
-mkdir success
-
-#for i in `egrep -A 1 Result: * 2>/dev/null | grep success | awk '{print $1}' | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g | sort -u`; do mv $i success/; done
-for i in `for j in $(ls); do egrep -A 1 Result: $j 2>/dev/null -H | egrep "success|pending" | awk '{print $1}' | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g | sort -u; done`; do mv $i success/; done
+# For each *.html file, find successes (success|pending) and move them to success/
+for i in $(for j in $(ls); do grep -E -A 1 'Result:' "$j" 2>/dev/null -H | grep -E 'success|pending' | awk '{print $1}' | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g | sort -u; done); do
+  mv "$i" success/
+done
 
 mv *.html success/ 2>/dev/null
 
-
-
 # sort the failed tasks by type
+cd failure/ || exit 1
 
-cd failure/
+# Build list of unique labels (2nd field after 'Label')
+SORTLIST=$(grep -E -A 1 'Label' * 2>/dev/null | awk '{print $2}' | grep -Ev 'Label:' | sort -u | grep -E . | sed 's/^[ 	]*//;s/[ 	]*$//')
 
-#SORTLIST=`egrep -A 1 Label * 2>/dev/null | egrep -v "Label:|\-\-" | awk '{print $2}' | sort -u | grep ':'`
-SORTLIST=$(egrep -A 1 Label * 2>/dev/null | awk '{print $2}' | egrep -v Label: | sort -u | egrep . | sed 's/^[ \t]*//;s/[ \t]*$//')
+for j in $SORTLIST; do
+  MYDIR=$(echo "$j" | tr -d ':')
+  mkdir -p "$MYDIR"
 
-for j in $SORTLIST; do 
-
-	MYDIR=`echo $j | tr -d ':'`;
-	mkdir $MYDIR;
-
-	IFS=$'\n';for i in `egrep -A 1 Label: * 2>/dev/null | egrep $j | awk '{print $1}' | sort -u | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g`; do 
-		mv $i $MYDIR/; 
-	done;
-
+  IFS=$'\n'
+  for i in $(grep -E -A 1 'Label:' * 2>/dev/null | grep -E "$j" | awk '{print $1}' | sort -u | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g); do
+    mv "$i" "$MYDIR"/
+  done
 done
 
 # sort the successful tasks by type
+cd ../success || exit 1
 
-cd ../success
-
-#SORTLIST=`egrep -A 1 Label * 2>/dev/null | egrep -v "Label:|\-\-" | awk '{print $2}' | sort -u | grep ':'`
-SORTLIST=$(egrep -A 1 Label * 2>/dev/null | awk '{print $2}' | egrep -v Label: | sort -u | egrep . | sed 's/^[ \t]*//;s/[ \t]*$//')
+SORTLIST=$(grep -E -A 1 'Label' * 2>/dev/null | awk '{print $2}' | grep -Ev 'Label:' | sort -u | grep -E . | sed 's/^[ 	]*//;s/[ 	]*$//')
 
 for j in $SORTLIST; do
+  MYDIR=$(echo "$j" | tr -d ':')
+  mkdir -p "$MYDIR"
 
-        MYDIR=`echo $j | tr -d ':'`;
-        mkdir $MYDIR;
-
-        IFS=$'\n';for i in `egrep -A 1 Label: * 2>/dev/null | egrep $j | awk '{print $1}' | sort -u | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g`; do
-                mv $i $MYDIR/;
-        done;
-
+  IFS=$'\n'
+  for i in $(grep -E -A 1 'Label:' * 2>/dev/null | grep -E "$j" | awk '{print $1}' | sort -u | sed s'/.html-/.html/'g | sed s'/.html:/.html/'g); do
+    mv "$i" "$MYDIR"/
+  done
 done
